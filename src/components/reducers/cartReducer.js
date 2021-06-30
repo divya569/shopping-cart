05264d8320fb0319ceffcb1584b5be3e4cdd203a
divya <divya569@sasi.ac.in>
2021-06-30 22:12:41 +0530

    cart actions

---
+++ src/components/reducers/cartReducer.js
@@ -0,0 +1,116 @@
+import Item1 from '../../images/item1.jpg'
+import Item2 from '../../images/item2.jpg'
+import Item3 from '../../images/item3.jpg'
+import Item4 from '../../images/item4.jpg'
+import Item5 from '../../images/item5.jpg'
+import Item6 from '../../images/item6.jpg'
+import { ADD_TO_CART,REMOVE_ITEM,SUB_QUANTITY,ADD_QUANTITY,ADD_SHIPPING } from '../actions/action-types/cart-actions'
+
+
+const initState = {
+    items:[  {id:1,title:'Samsung', desc: "Samsung Galaxy Watch Active 2 (Bluetooth, 44 mm) - Silver, Aluminium Dial, Silicon Straps", price:255,img:Item1},
+    {id:2,title:'Titan', desc: "Titan Analog Grey Dial Women's Watch-2651WM01", price:75,img: Item2},
+    {id:3,title:'Sonata', desc: "Sonata Blush It Up Analog Red Dial Women's Watch NM8123WM02/NN8123WM02",price:40,img: Item3},
+    {id:4,title:'FastTrack', desc: "Fastrack Autumn-Winter 20 Analog Black Dial Women's Watch-6207NM01", price:45,img:Item4},
+    {id:5,title:'MI', desc: "Mi Smart Band 5 – India’s No. 1 Fitness Band, 1.1-inch AMOLED Color Display, Magnetic Charging, 2 Weeks Battery Life, Personal Activity Intelligence (PAI), Women’s Health Tracking", price:35,img: Item5},
+    {id:6,title:'Amafit', desc: "Amazfit GTS2 Mini Smart Watch with 1.55 AMOLED Display, SpO2 Level Measurement, 14 Days' Battery Life, 70+ Sports Modes, Built-in Amazon Alexa & GPS, HR, Sleep&Stress Monitoring(Flamingo Pink)",price:100,img: Item6}
+],
+    addedItems:[],
+    total: 0
+
+}
+const cartReducer= (state = initState,action)=>{
+   
+    //INSIDE HOME COMPONENT
+    if(action.type === ADD_TO_CART){
+          let addedItem = state.items.find(item=> item.id === action.id)
+          //check if the action id exists in the addedItems
+         let existed_item= state.addedItems.find(item=> action.id === item.id)
+         if(existed_item)
+         {
+            addedItem.quantity += 1 
+             return{
+                ...state,
+                 total: state.total + addedItem.price 
+                  }
+        }
+         else{
+            addedItem.quantity = 1;
+            //calculating the total
+            let newTotal = state.total + addedItem.price 
+            
+            return{
+                ...state,
+                addedItems: [...state.addedItems, addedItem],
+                total : newTotal
+            }
+            
+        }
+    }
+    if(action.type === REMOVE_ITEM){
+        let itemToRemove= state.addedItems.find(item=> action.id === item.id)
+        let new_items = state.addedItems.filter(item=> action.id !== item.id)
+        
+        //calculating the total
+        let newTotal = state.total - (itemToRemove.price * itemToRemove.quantity )
+        console.log(itemToRemove)
+        return{
+            ...state,
+            addedItems: new_items,
+            total: newTotal
+        }
+    }
+    //INSIDE CART COMPONENT
+    if(action.type=== ADD_QUANTITY){
+        let addedItem = state.items.find(item=> item.id === action.id)
+          addedItem.quantity += 1 
+          let newTotal = state.total + addedItem.price
+          return{
+              ...state,
+              total: newTotal
+          }
+    }
+    if(action.type=== SUB_QUANTITY){  
+        let addedItem = state.items.find(item=> item.id === action.id) 
+        //if the qt == 0 then it should be removed
+        if(addedItem.quantity === 1){
+            let new_items = state.addedItems.filter(item=>item.id !== action.id)
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                addedItems: new_items,
+                total: newTotal
+            }
+        }
+        else {
+            addedItem.quantity -= 1
+            let newTotal = state.total - addedItem.price
+            return{
+                ...state,
+                total: newTotal
+            }
+        }
+        
+    }
+
+    if(action.type=== ADD_SHIPPING){
+          return{
+              ...state,
+              total: state.total + 6
+          }
+    }
+
+    if(action.type=== 'SUB_SHIPPING'){
+        return{
+            ...state,
+            total: state.total - 6
+        }
+  }
+    
+  else{
+    return state
+    }
+    
+}
+
+export default cartReducer
